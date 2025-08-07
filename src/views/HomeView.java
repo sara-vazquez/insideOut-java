2ee3c38c7905df91ae9576257c0062fd4b264dcf
Sara Vázquez <saravazlo@gmail.com>
2025-08-07 15:25:34 +0200

    feat-views

--- src/views/HomeView.java
+++ src/views/HomeView.java
@@ -1,3 +1,24 @@
-public class HomeView {
+package dev.sara.views;
+public class HomeView extends View {
+
+    public static void printMenu() {
+
+        String text = """
+                My diario:
+                1. Añadir momento
+                2. Ver todos los momentos disponibles
+                3. Eliminar un momento
+                4. Filtrar los momentos
+                5. Salir
+                ----------------------
+                Seleccione una opción:
+                """;
+        
+        System.out.print(text);
+
+        int option = SCANNER.nextInt();
+
+        if (option == 1) MomentPostView.printStoreMenu();
+    }
     
 }
