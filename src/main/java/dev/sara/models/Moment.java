907697e773e939950743c40ab339885a90434094
Sara Vázquez <saravazlo@gmail.com>
2025-08-11 16:48:44 +0200

    feat-updated moment

--- src/main/java/dev/sara/models/Moment.java
+++ src/main/java/dev/sara/models/Moment.java
@@ -1,7 +1,56 @@
 package dev.sara.models;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Moment {
+
+    private int id;
     private String momentTitle;
     private String momentDescription;
-    
+    private Emotion emotion;
+    private LocalDate momentDate;
+    private LocalDateTime creationDate;
+    private LocalDateTime modDate;
+
+    public Moment(String momentTitle, String momentDescription, Emotion emotion, LocalDate momentDate) {
+        this.id = nextId++;
+        this.momentTitle = momentTitle;
+        this.momentDescription = momentDescription;
+        this.emotion = emotion;
+        this.momentDate = momentDate;
+
+        this.creationDate = LocalDateTime.now();
+        this.modDate = LocalDateTime.now();
+    }
+
+    //Getters
+
+    public int getId() {
+        return id;
+    } 
+
+    public String getMomentTitle() {
+        return momentTitle;
+    } 
+
+    public String getMomentDescription() {
+        return momentDescription;
+    } 
+
+    public Emotion getEmotion() {
+        return emotion;
+    } 
+
+    public LocalDate getMomentDate() {
+        return momentDate;
+    } 
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    } 
+
+    public LocalDateTime getModDate() {
+        return modDate;
+    } 
 }
