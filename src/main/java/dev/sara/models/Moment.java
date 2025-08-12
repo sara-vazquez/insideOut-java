7b5c3cbf2789ce787cf1e4c26b5ac9a5fde64f81
Sara Vázquez <saravazlo@gmail.com>
2025-08-12 12:04:38 +0200

    updated

--- src/main/java/dev/sara/models/Moment.java
+++ src/main/java/dev/sara/models/Moment.java
@@ -14,7 +14,7 @@ public class Moment {
     private LocalDateTime modDate;
 
     public Moment(String momentTitle, String momentDescription, Emotion emotion, LocalDate momentDate) {
-        this.id = nextId++;
+        this.id = id;
         this.momentTitle = momentTitle;
         this.momentDescription = momentDescription;
         this.emotion = emotion;
@@ -24,33 +24,61 @@ public class Moment {
         this.modDate = LocalDateTime.now();
     }
 
-    //Getters
-
     public int getId() {
         return id;
-    } 
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getMomentTitle() {
         return momentTitle;
-    } 
+    }
+
+    public void setMomentTitle(String momentTitle) {
+        this.momentTitle = momentTitle;
+    }
 
     public String getMomentDescription() {
         return momentDescription;
-    } 
+    }
+
+    public void setMomentDescription(String momentDescription) {
+        this.momentDescription = momentDescription;
+    }
 
     public Emotion getEmotion() {
         return emotion;
-    } 
+    }
+
+    public void setEmotion(Emotion emotion) {
+        this.emotion = emotion;
+    }
 
     public LocalDate getMomentDate() {
         return momentDate;
-    } 
+    }
+
+    public void setMomentDate(LocalDate momentDate) {
+        this.momentDate = momentDate;
+    }
 
     public LocalDateTime getCreationDate() {
         return creationDate;
-    } 
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
 
     public LocalDateTime getModDate() {
         return modDate;
-    } 
+    }
+
+    public void setModDate(LocalDateTime modDate) {
+        this.modDate = modDate;
+    }
+
+     
 }
