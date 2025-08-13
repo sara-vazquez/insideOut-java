e3de5e13b319efcba1f00425b4e777bf97d7bb6e
Sara Vázquez <saravazlo@gmail.com>
2025-08-13 20:01:30 +0200

    feat-controller updated

--- src/main/java/dev/sara/controllers/MomentController.java
+++ src/main/java/dev/sara/controllers/MomentController.java
@@ -13,8 +13,16 @@ import java.util.List;
 public class MomentController {
     private static MomentRepository REPOSITORY = MomentRepositorySingleton.getInstance();
 
-    public void StoreMoment(MomentDTO momentDTO) {
-        Moment momentToSave = MomentMapper.toEntity(momentDTO);
-        repository.StoreMoment(momentToSave);
+    public static void addMoment() {
+        MomentDTO newMomentDTO = MomentPostView.getMomentData();
+        Moment momentToSave = MomentMapper.toEntity(newMomentDTO);
+        REPOSITORY.save(momentToSave);
+        System.out.println("✅ Momento añadido con éxito.");
+    }
+
+    public void ViewAllMoments() {
+        List<Moment> moments = REPOSITORY.findAll();
+        List<MomentDTO> momentsDTO = MomentMapper.toDTOList(moments);
+        HomeView.printMomentList(momentsDTO);
     }
 }
