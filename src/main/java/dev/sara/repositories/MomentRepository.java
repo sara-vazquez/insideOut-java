4e3a292d323de2b9992e428e6da60a8d41c1590e
Sara Vázquez <saravazlo@gmail.com>
2025-08-13 19:11:35 +0200

    feat-moment repository updated

--- src/main/java/dev/sara/repositories/MomentRepository.java
+++ src/main/java/dev/sara/repositories/MomentRepository.java
@@ -2,19 +2,49 @@ package dev.sara.repositories;
 
 import dev.sara.db.*;
 import dev.sara.contracts.*;
+import dev.sara.singletons.*;
 import dev.sara.models.*;
+import java.util.Deque; //para poder hacer LIFO
+import java.util.LinkedList;
+import java.util.List;
 
 public class MomentRepository {
 
     private InterfaceDatabase db;
+    private final Deque<Moment> momentStore = new LinkedList<>();
 
-    public MomentRepository() {
-        this.db = new DiaryDatabaseTest();
+    //Constructor privado para el sigleton
+    private MomentRepository() {
+        this.db = new DiaryDatabase();
+    }
+    
+
+    public static MomentRepository getInstance() {
+        return MomentRepositorySingleton.getInstance();
     }
     
     public void StoreMoment(Moment moment) {
 
         db.store(moment);
+        momentStore.push(moment);
+    }
+
+    public List<Moment> findAll() {
+        return (List<Moment>) momentStore;
+    }
+
+    public Moment findLast() {
+        if (momentStore.isEmpty()) {
+            return null;
+        }
+        return momentStore.peek(); //para ver el último momento añadido
+    }
+    
+    public Moment deleteLast() {
+        if (momentStore.isEmpty()) {
+            return null;
+        }
+        return momentStore.pop();
     }
 
 }
