ebdfc84012cefda2c4a62782a09ee1570c6c9854
Sara Vázquez <saravazlo@gmail.com>
2025-08-21 14:00:19 +0200

    fix-shor†info

--- src/main/java/dev/sara/movies/vo/shortInfoVO.java
+++ src/main/java/dev/sara/movies/vo/shortInfoVO.java
@@ -17,6 +17,16 @@ public class shortInfoVO {
         }
     
         @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((name == null) ? 0 : name.hashCode());
+            result = prime * result + Arrays.hashCode(genre);
+            result = prime * result + ((releaseYear == null) ? 0 : releaseYear.hashCode());
+            return result;
+        }
+
+        @Override
         public boolean equals(Object obj) {
             if (this == obj)
                 return true;
@@ -24,13 +34,18 @@ public class shortInfoVO {
                 return false;
             if (getClass() != obj.getClass())
                 return false;
-            ShortInfoVO other = (ShortInfoVO) obj;
+            shortInfoVO other = (shortInfoVO) obj;
             if (name == null) {
                 if (other.name != null)
                     return false;
             } else if (!name.equals(other.name))
                 return false;
             if (!Arrays.equals(genre, other.genre))
+                return false;
+            if (releaseYear == null) {
+                if (other.releaseYear != null)
+                    return false;
+            } else if (!releaseYear.equals(other.releaseYear))
                 return false;
             return true;
         }
