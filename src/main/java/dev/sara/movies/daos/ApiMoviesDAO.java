dba78c3bb917214f1b92ae1e06de5acc1fcc73a3
Sara Vázquez <saravazlo@gmail.com>
2025-08-21 14:18:26 +0200

    feat-movieservice done

--- src/main/java/dev/sara/movies/daos/ApiMoviesDAO.java
+++ src/main/java/dev/sara/movies/daos/ApiMoviesDAO.java
@@ -5,6 +5,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 
 public class ApiMoviesDAO implements InterfaceApiMovieDAO {
