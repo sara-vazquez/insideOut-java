34f2dbccdc30d182fc83635aacf1c4213d4d9ae3
Sara Vázquez <saravazlo@gmail.com>
2025-08-21 14:13:02 +0200

    feat-model movie created

--- src/main/java/dev/sara/movies/models/Movie.java
+++ src/main/java/dev/sara/movies/models/Movie.java
@@ -1,5 +1,71 @@
 package dev.sara.movies.models;
 
+import dev.sara.models.Emotion;
+
 public class Movie {
     
+    private int imdbId;
+    private String name;
+    private String[] genre;
+    private Emotion emotion;
+    private String releaseYear;
+    private String creationDate;
+
+    public Movie(int imdbId, String name, String []genre, Emotion emotion, String releaseYear, String creationDate) {
+        this.imdbId = imdbId; 
+        this.name = name;
+        this.genre = genre;
+        this.emotion = emotion;
+        this.releaseYear = releaseYear;
+        this.creationDate = creationDate;
+    }
+
+    public int getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(int imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String[] genre) {
+        this.genre = genre;
+    }
+
+    public Emotion getEmotion() {
+        return emotion;
+    }
+
+    public void setEmotion(Emotion emotion) {
+        this.emotion = emotion;
+    }
+
+    public String getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(String releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
 }
